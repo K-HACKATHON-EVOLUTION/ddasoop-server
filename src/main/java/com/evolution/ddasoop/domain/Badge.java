2e7cda7113c0545408515025ca37889dc0c1d881
yhjune <yhjune09@gmail.com>
2021-08-10 13:42:36 +0900

    entity

---
+++ src/main/java/com/evolution/ddasoop/domain/Badge.java
@@ -0,0 +1,12 @@
+package com.evolution.ddasoop.domain;
+
+public class Badge {
+
+    private Integer userIdx;
+
+    private Integer badgeIdx;
+
+    private String badgeImg;
+
+    private Integer treeIdx;
+}
---
+++ src/main/java/com/evolution/ddasoop/domain/Log.java
@@ -0,0 +1,22 @@
+package com.evolution.ddasoop.domain;
+
+import java.sql.Timestamp;
+
+public class Log {
+    private Integer userIdx;
+
+    private Integer courseIdx;
+
+    private String courseName;
+
+    // 코스 수식어?
+    private String courseIntro;
+
+    private String theme;
+
+    private Timestamp courseDate;
+
+    private Boolean deleteFlag;
+
+    private Integer distance;
+}
