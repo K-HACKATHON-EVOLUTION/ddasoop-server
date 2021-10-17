43500d9b579149415216c877d2fb89e5664e1172
hak2711 <hak271111@gmail.com>
2021-10-17 22:18:57 +0900

    Feat: 추가 내가 저장한 코스 기능

--- src/main/java/com/evolution/ddasoop/domain/HeartRepository.java
+++ src/main/java/com/evolution/ddasoop/domain/HeartRepository.java
@@ -3,8 +3,11 @@ package com.evolution.ddasoop.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HeartRepository extends JpaRepository<Heart, Long> {
     Heart findHeartByCourseCourseIdxAndUserUserIdxAndDeleteFlagFalse(Long courseIdx, String userIdx);
     Integer countHeartByCourseAndDeleteFlagFalse(Course course);
+    List<Heart> findAllByUserUserIdxAndDeleteFlagFalse(String userIdx);
 }
